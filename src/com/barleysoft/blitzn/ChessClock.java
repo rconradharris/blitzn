aa1b901c9f4f52b8e14670884bb4be61573dfa63
Rick Harris <rconradharris@gmail.com>
2010-10-02 18:30:15 -0500

    initial directory structure

---
+++ src/com/barleysoft/blitzn/ChessClock.java
@@ -0,0 +1,13 @@
+package com.barleysoft.blitzn;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ChessClock extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
